ec04219a5844ba51728ab2c664d5f62dde2855e3
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-22 17:37:49 +0400

    integrated the dropdown menu from headless UI

--- components/navbar/UserMenu.vue
+++ components/navbar/UserMenu.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+
 import { useMainStore } from "~/stores/store";
 const { data, signOut } = useAuth();
 
@@ -8,60 +10,88 @@ const currentUser = computed(() => {
   return data.value;
 });
 
-import MenuItem from "~/components/navbar/MenuItem.vue";
+// import MenuItem from "~/components/navbar/MenuItem.vue";
 import Avatar from "~/components/Avatar.vue";
 import HamburgerIcon from "~/components/ui/icons/HamburgerIcon.vue";
 
 const router = useRouter();
 const isOpen = ref(false);
+const menuItemClass =
+  "px-4 py-3 text-sm hover:bg-neutral-100 transition font-semibold inline-block w-full";
 
 const toggleOpen = () => {
   isOpen.value = !isOpen.value;
 };
+
+const links = [
+  { href: "/trips", label: "My trips" },
+  { href: "/favorites", label: "My favorites" },
+  { href: "/reservations", label: "My reservations" },
+  { href: "", label: "Airbnb your home" },
+];
 </script>
 
 <template>
   <div class="relative">
-    <div class="flex flex-row items-center gap-3">
+    <div class="flex flex-row items-center gap-3 relative">
       <div
         click="onRent"
         class="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
       >
         Airbnb your home
       </div>
-      <div
-        @click="toggleOpen"
-        class="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
-      >
-        <HamburgerIcon class="w-4 h-4 flex-shrink-0" />
-        <div class="hidden md:block">
-          <Avatar :src="currentUser?.user?.image" class="max-w-[30px]" />
-        </div>
-      </div>
-    </div>
-
-    <div
-      v-if="isOpen"
-      class="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm"
-    >
-      <div class="flex flex-col cursor-pointer">
-        <template v-if="currentUser">
-          <MenuItem label="My trips" @click="router.push('/trips')" />
-          <MenuItem label="My favorites" @click="router.push('/favorites')" />
-          <MenuItem
-            label="My reservations"
-            @click="router.push('/reservations')"
-          />
-          <MenuItem label="My properties" @click="router.push('/properties')" />
-          <MenuItem label="Airbnb your home" @click="" />
-          <hr />
-          <MenuItem label="Logout" @click="signOut()" />
-        </template>
-        <template v-else>
-          <MenuItem label="Login" @click="setSigninOpen(true)" />
-          <MenuItem label="Sign up" @click="setSignupOpen(true)" />
-        </template>
-      </div>
+      <ClientOnly>
+        <Menu as="div" class="relative inline-block text-left">
+          <MenuButton>
+            <div
+              @click="toggleOpen"
+              class="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+            >
+              <HamburgerIcon class="w-4 h-4 flex-shrink-0" />
+              <div class="hidden md:block">
+                <Avatar :src="currentUser?.user?.image" class="max-w-[30px]" />
+              </div>
+            </div>
+          </MenuButton>
+          <transition
+            enter-active-class="transition duration-100 ease-out"
+            enter-from-class="transform scale-95 opacity-0"
+            enter-to-class="transform scale-100 opacity-100"
+            leave-active-class="transition duration-75 ease-in"
+            leave-from-class="transform scale-100 opacity-100"
+            leave-to-class="transform scale-95 opacity-0"
+          >
+            <MenuItems
+              class="absolute right-0 mt-2 w-48 origin-top-right rounded-xl bg-white shadow-md ring-1 ring-black/5 focus:outline-none"
+            >
+              <!-- Use the `active` state to conditionally style the active item. -->
+              <template v-if="currentUser">
+                <MenuItem
+                  v-for="link in links"
+                  :key="link.href"
+                  :href="link.href"
+                  :as="link.href ? 'a' : 'li'"
+                  :class="menuItemClass"
+                >
+                  {{ link.label }}
+                </MenuItem>
+              </template>
+              <template v-else>
+                <MenuItem
+                  as="li"
+                  :class="menuItemClass"
+                  @click="setSigninOpen(true)"
+                />
+                <MenuItem
+                  as="li"
+                  :class="menuItemClass"
+                  @click="setSignupOpen(true)"
+                />
+              </template>
+            </MenuItems>
+          </transition>
+        </Menu>
+      </ClientOnly>
     </div>
   </div>
 </template>
