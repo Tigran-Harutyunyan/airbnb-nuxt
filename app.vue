f93915a91563bf3a643a9a9720b23e6c61d065a0
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-22 16:45:50 +0400

    cleaned up  the app component from test data

--- app.vue
+++ app.vue
@@ -1,22 +1,12 @@
 <template>
-  <div class="max-w-5xl mx-auto mt-5 px-5">
-    <h3 class="text-xl font-bold">Authentication Overview.</h3>
-    <p class="text-sm">
-      See all available authentication & session information below.
-    </p>
-    <pre v-if="status"><span>Status:</span> {{ status }}</pre>
-    <pre v-if="data"><span>Data:</span> {{ data }}</pre>
-    <pre v-if="csrfToken"><span>CSRF Token:</span> {{ csrfToken }}</pre>
-    <pre v-if="providers"><span>Providers:</span> {{ providers }}</pre>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 
-    <LoginModal v-if="status != 'authenticated'" />
-
-    <button v-else @click="signOut()" class="bg-blue-200 p-3">Logout</button>
-  </div>
+  <ModalsContainer v-if="status != 'authenticated'" />
 </template>
 <script setup lang="ts">
-const { data, status, signOut, getCsrfToken, getProviders } = useAuth();
-import LoginModal from "./components/modals/LoginModal.vue";
-const providers = await getProviders();
-const csrfToken = await getCsrfToken();
+import ModalsContainer from "./components/modals/ModalsContainer.vue";
+
+const { status } = useAuth();
 </script>
