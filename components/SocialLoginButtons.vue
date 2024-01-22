5928ce33f36982cd9e9daf86f3b4a55ca9508266
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-22 16:45:09 +0400

    created social login buttons component that will be shared across login and register  modals

---
+++ components/SocialLoginButtons.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Button from "./Button.vue";
+import GithubIcon from "./ui/icons/GithubIcon.vue";
+import GoogleIcon from "./ui/icons/GoogleIcon.vue";
+
+const { signIn } = useAuth();
+</script>
+
+<template>
+  <Button outline label="Continue with Google" @click="signIn('google')">
+    <GoogleIcon />
+  </Button>
+  <Button outline label="Continue with Github" @click="signIn('github')">
+    <GithubIcon />
+  </Button>
+</template>
