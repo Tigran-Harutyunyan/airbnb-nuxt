27b116cab091675d88dc96b5c038394c1e518656
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-22 16:41:04 +0400

    created initial version of navbar

---
+++ components/navbar/Navbar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+//import Categories from "~/components/Categories";
+import Container from "~/components/Container.vue";
+import Logo from "~/components/navbar/Logo.vue";
+// import Search from "~/components/Search";
+import UserMenu from "~/components/navbar/UserMenu.vue";
+</script>
+
+<template>
+  <div class="fixed w-full bg-white z-10 shadow-sm">
+    <div class="py-4 border-b-[1px]">
+      <Container>
+        <div class="flex flex-row items-center justify-between gap-3 md:gap-0">
+          <Logo />
+          <!-- <Search /> -->
+          <UserMenu />
+        </div>
+      </Container>
+    </div>
+    <!-- <Categories /> -->
+  </div>
+</template>
