f59393db549e4168aa67938f3b5321014ffe0684
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-22 16:41:25 +0400

    created default layout

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Navbar from "../components/navbar/Navbar.vue";
+</script>
+<template>
+  <Navbar />
+  <div className="pb-20 pt-28">
+    <slot />
+  </div>
+</template>
