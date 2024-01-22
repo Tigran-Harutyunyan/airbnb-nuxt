6292380fc225c43dabf9954f4444e002ec52a5b3
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-22 16:59:14 +0400

    updated the package json and configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@sidebase/nuxt-auth'],
+  modules: ['@nuxtjs/tailwindcss', '@sidebase/nuxt-auth', '@pinia/nuxt', "@nuxt/image"],
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=500, initial-scale=1',
+      title: 'Airbnb with Nuxt3',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { name: 'description', content: 'Airbnb with Nuxt.js.' },
+
+        { name: 'keywords', content: 'Nuxt Airbnb' },
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Raleway:wght@400;500;600;700&display=swap' }
+      ]
+    },
+  },
   auth: {
     provider: {
       type: 'authjs'
