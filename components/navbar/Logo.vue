3ee06d40c75bdcb3a072b5537f69bce2a18afc49
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-22 16:34:26 +0400

    added Logo component

---
+++ components/navbar/Logo.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+//import { useRouter } from "next/navigation";
+const router = useRouter();
+</script>
+
+<template>
+  <NuxtImg
+    @click="router.push('/')"
+    class="hidden md:block cursor-pointer"
+    src="/images/logo.png"
+    height="31"
+    width="100"
+    alt="Logo"
+  />
+</template>
