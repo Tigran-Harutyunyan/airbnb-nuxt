72e02ebd14e354e2a60f2068b2cd596551494b4d
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-22 16:38:54 +0400

    added a avatar ccomponent

---
+++ components/Avatar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { src } = defineProps<{
+  src: string | null | undefined;
+}>();
+</script>
+
+<template>
+  <NuxtImg
+    class="rounded-full"
+    height="30"
+    width="30"
+    alt="Avatar"
+    :src="src || '/images/placeholder.jpg'"
+  />
+</template>
