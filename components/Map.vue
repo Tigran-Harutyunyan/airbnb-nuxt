cc566b8718dd27f09ff7c448ed5d830b6e129b13
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-24 10:54:15 +0400

    created a Map component that integrates the leaflet map

---
+++ components/Map.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const { center } = defineProps<{
+  center: number[];
+}>();
+
+const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const zoom = ref(center ? 4 : 2);
+</script>
+
+<template>
+  <div class="h-[35vh] rounded-lg">
+    <LMap
+      ref="map"
+      :zoom="zoom"
+      :center="center || [51, -0.09]"
+      class="h-[35vh] rounded-lg"
+    >
+      <LTileLayer
+        :url="url"
+        :attribution="attribution"
+        layer-type="base"
+        name="OpenStreetMap"
+      />
+      <LMarker :lat-lng="center" v-if="center" />
+    </LMap>
+  </div>
+</template>
