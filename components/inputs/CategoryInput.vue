bc9fbad86b4c5251465e9fb17722dc8a498bd730
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-24 11:00:50 +0400

    created a category selector that integrates in Rent modal

---
+++ components/inputs/CategoryInput.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const { icon, label, selected } = defineProps<{
+  icon: any;
+  label: string;
+  selected: boolean;
+}>();
+const emit = defineEmits(["click"]);
+</script>
+
+<template>
+  <div
+    @click="emit('click', label)"
+    :class="`
+        rounded-xl
+        border-2
+        p-4
+        flex
+        flex-col
+        gap-3
+        hover:border-black
+        transition
+        cursor-pointer
+        ${selected ? 'border-black' : 'border-neutral-200'}
+      `"
+  >
+    <component :is="icon" class="w-[30px] h-[30px]" />
+    <div class="font-semibold">
+      {{ label }}
+    </div>
+  </div>
+</template>
