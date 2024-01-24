e0c1bcd805f3936d63d57fb42f1a6aac72c2bf3e
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-24 10:52:52 +0400

    created icons

---
+++ components/ui/icons/categories/TbPool.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg
+    stroke="currentColor"
+    fill="none"
+    stroke-width="2"
+    viewBox="0 0 24 24"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    height="26"
+    width="26"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path
+      d="M2 20a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1"
+    ></path>
+    <path
+      d="M2 16a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1"
+    ></path>
+    <path d="M15 12v-7.5a1.5 1.5 0 0 1 3 0"></path>
+    <path d="M9 12v-7.5a1.5 1.5 0 0 0 -3 0"></path>
+    <path d="M15 5l-6 0"></path>
+    <path d="M9 10l6 0"></path>
+  </svg>
+</template>
