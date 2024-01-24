5ae1ee330f7d5bf0fe598d0263fd90f475c0e2f5
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-24 10:59:47 +0400

    added a composable that returns the list of countries

---
+++ composables/useCountries.js
@@ -0,0 +1,21 @@
+import countries from "world-countries";
+export const useCountries = () => {
+  const formattedCountries = countries.map((country) => ({
+    value: country.cca2,
+    label: country.name.common,
+    flag: country.flag,
+    latlng: country.latlng,
+    region: country.region,
+  }));
+
+  const getAll = () => formattedCountries;
+
+  const getByValue = (value) => {
+    return formattedCountries.find((item) => item.value === value);
+  };
+
+  return {
+    getAll,
+    getByValue,
+  };
+};
