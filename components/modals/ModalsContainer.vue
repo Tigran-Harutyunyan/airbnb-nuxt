8aa8c640270047025dba094f52a75cd05b2bc35d
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-24 10:56:07 +0400

    added a Rent modal to  modals container

--- components/modals/ModalsContainer.vue
+++ components/modals/ModalsContainer.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import LoginModal from "./LoginModal.vue";
 import RegisterModal from "./RegisterModal.vue";
+import RentModal from "./RentModal.vue";
+
+const { status } = useAuth();
 </script>
 
 <template>
-  <LoginModal />
-  <RegisterModal />
+  <template v-if="status != 'authenticated'">
+    <LoginModal />
+    <RegisterModal />
+  </template>
+
+  <RentModal />
 </template>
