29386600bdfa4312f44fba06131640b1d8fd8188
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-25 10:19:51 +0400

    added 404 page

---
+++ pages/[...404].vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="mx-auto w-full flex items-center justify-center px-2 h-screen">
+    <div class="flex flex-col items-center my-auto">
+      <img src="/images/404.gif" class="w-40" />
+
+      <p class="my-4 text-center font-bold">
+        Ooops! We can't seem to find the page you're looking for.
+      </p>
+      <NuxtLink to="/" class="w-full mt-2">
+        <Button label="BACK TO HOMEPAGE"></Button>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
