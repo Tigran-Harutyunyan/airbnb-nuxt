c90c910f535bbc059ddfc667cf8600880f1036a1
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-26 12:54:12 +0400

    finished the listings page

--- pages/index.vue
+++ pages/index.vue
@@ -3,21 +3,33 @@ import EmptyState from "~/components/EmptyState.vue";
 import Container from "~/components/Container.vue";
 import ListingCard from "~/components/listings/ListingCard.vue";
 import { useMainStore } from "~/stores/store";
+import { useStringifiedUrl } from "~/composables/useStringifiedUrl";
+import { type SafeListing } from "~/types";
+
+const route = useRouter();
 
 const { currentUser } = storeToRefs(useMainStore());
 
-const isLoading = ref(true);
+const { getUrl } = useStringifiedUrl("/api/listings");
 
-const { data } = await useFetch("/api/listings", {
+const { data: listings, refresh } = await useFetch<SafeListing[]>(getUrl, {
   method: "get",
 });
 
-isLoading.value = false;
+watch(
+  () => route,
+  (value) => {
+    refresh();
+  }
+);
 </script>
 
 <template>
   <ClientOnly>
-    <EmptyState showReset v-if="!data" />
+    <EmptyState
+      showReset
+      v-if="!listings || (Array.isArray(listings) && listings?.length === 0)"
+    />
   </ClientOnly>
 
   <Container>
@@ -25,7 +37,7 @@ isLoading.value = false;
       class="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8"
     >
       <ListingCard
-        v-for="listing in data"
+        v-for="listing in listings"
         :currentUser="currentUser"
         :key="listing.id"
         :data="listing"
