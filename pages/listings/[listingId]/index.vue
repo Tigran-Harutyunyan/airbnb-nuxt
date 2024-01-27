a62094e4f308a1a9a654277baf27f4603e002f7e
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-27 14:56:09 +0400

    created listing view page

---
+++ pages/listings/[listingId]/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import ListingClient from "~/components/listings/ListingClient.vue";
+import EmptyState from "~/components/EmptyState.vue";
+const { params } = useRoute();
+
+const { data: listing } = await useFetch(`/api/listings/${params.listingId}`);
+</script>
+
+<template>
+  <template v-if="!listing">
+    <ClientOnly>
+      <EmptyState />
+    </ClientOnly>
+  </template>
+
+  <ClientOnly v-else>
+    <ListingClient :listing="listing" />
+  </ClientOnly>
+</template>
