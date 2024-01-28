774f2a65932c474a3608688af9792e0f1ae01657
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-28 13:06:33 +0400

    create a favorite listings page

---
+++ pages/favorites/index.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import EmptyState from "~/components/EmptyState.vue";
+import FavoritesClient from "~/components/FavoritesClient.vue";
+import { useMainStore } from "~/stores/store";
+
+const { currentUser, favouritesCount } = storeToRefs(useMainStore());
+
+const isLoading = ref(false);
+
+const listings = ref([]);
+
+const getFavorites = async () => {
+  isLoading.value = true;
+  const response = await $fetch("/api/favorites");
+
+  if (response && Array.isArray(response)) {
+    listings.value = response;
+  }
+  isLoading.value = false;
+};
+
+getFavorites();
+
+watch(
+  () => favouritesCount.value,
+  () => {
+    getFavorites();
+  }
+);
+</script>
+
+<template>
+  <ClientOnly v-if="!currentUser">
+    <EmptyState title="Unauthorized" subtitle="Please login" />
+  </ClientOnly>
+
+  <ClientOnly v-if="currentUser && !listings.length && !isLoading">
+    <EmptyState
+      title="No favorites found"
+      subtitle="Looks like you have no favorite listings."
+    />
+  </ClientOnly>
+
+  <FavoritesClient
+    v-if="listings.length"
+    :listings="listings"
+    :currentUser="currentUser"
+  />
+</template>
