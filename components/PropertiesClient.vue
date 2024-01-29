2ec8f7f31f6c53e60e5711ff913c39bb2228ab39
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-29 10:10:03 +0400

    changed the description of section

--- components/PropertiesClient.vue
+++ components/PropertiesClient.vue
@@ -22,10 +22,7 @@ const onCancel = (id: string) => {
 
 <template>
   <Container>
-    <Heading
-      title="Trips"
-      subtitle="Where you've been and where you're going"
-    />
+    <Heading title="Properties" subtitle="List of your properties" />
     <div
       class="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8"
     >
